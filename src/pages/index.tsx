14e3c0b66694d26ccd5a8e283f392b72110fcbab
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-11-14 18:40:04 -0300

    first arrangement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,47 @@
 import Head from 'next/head'
+import { FormEvent, useState } from 'react'
 
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      email,
+      password
+    }
+
+    console.log(data)
+  }
+
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
 
-      <main className={styles.container}>
-        <h1>Hello world</h1>
-      </main>
+      <form onSubmit={handleSubmit} className={styles.container}>
+        <input 
+          type="email"
+          placeholder="Email"
+          value={email} 
+          onChange={e => setEmail(e.target.value)} 
+        />
+        <input 
+          type="password" 
+          placeholder="Senha"
+          value={password} 
+          onChange={e => setPassword(e.target.value)} 
+        />
+
+        <button type="submit">
+          Entrar
+        </button>
+      </form>
     </>
   )
 }
